733cc9ce1f53d979994cb3c0d50dfa021670fb77
dj-dev-jsx <deejaybalila7@gmail.com>
2026-08-10 08:32:02 +0800

    Initial setup

---
+++ server/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+const corsOptions = {
+  origin: 'http://localhost:5173',
+};
+
+app.use(cors(corsOptions));
+
+app.get('/api', (req, res) => {
+  res.json({ message: 'API is running' });
+});
+
+app.listen(8080, () => {
+  console.log('Server is running on port 8080');
+});
+
